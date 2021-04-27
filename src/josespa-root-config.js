e4950774229e567caa7c30e021cf369e13b5a9af
Jose-Diago <jose.diago@ibm.com>
2021-04-27 19:38:19 +0800

    Removing moungRootParce, sticking with singlespa recommended setup, also adding authentication applicaiton.

--- src/josespa-root-config.js
+++ src/josespa-root-config.js
@@ -1,32 +1,29 @@
-import { registerApplication, start, mountRootParcel } from "single-spa";
-
-// registerApplication({
-//   name: "@josespa/navbar",
-//   app: () => System.import("@josespa/navbar"),
-//   activeWhen: ["/"]
-// });
+import { registerApplication, start } from 'single-spa';
 
 registerApplication({
-  name: "@josespa/employees",
-  app: () => System.import("@josespa/employees"),
-  activeWhen: ["/employees"]
+  name: '@josespa/navbar',
+  app: () => System.import('@josespa/navbar'),
+  activeWhen: ['/']
 });
 
 registerApplication({
-  name: "@josespa/departments",
-  app: () => System.import("@josespa/departments"),
-  activeWhen: ["/departments"]
+  name: '@josespa/auth',
+  app: () => System.import('@josespa/auth'),
+  activeWhen: ['/auth']
+});
+
+registerApplication({
+  name: '@josespa/employees',
+  app: () => System.import('@josespa/employees'),
+  activeWhen: ['/employees']
+});
+
+registerApplication({
+  name: '@josespa/departments',
+  app: () => System.import('@josespa/departments'),
+  activeWhen: ['/departments']
 });
 
 start({
   urlRerouteOnly: true,
-});
-
-const navbar = mountRootParcel(System.import('@josespa/navbar'), {
-  author: 'Jose Mari A. Diago',
-  domElement: document.getElementById('navbar')
-});
-
-navbar.mountPromise.then(() => {
-  // what do you want to do here after mounting?
 });
